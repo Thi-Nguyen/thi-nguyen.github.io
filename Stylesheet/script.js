faa39ae3610f6aeefdbb641f32d64df28071a62c
Thi Nguyen <n_thi@outlook.com>
2018-06-02 04:12:51 -0400

    Added content page with a sidebar with an active feature. If user clicks on an icon from the main page, it will bring the user to the correct section and corresponding tab highlight the tab for it.

--- Stylesheet/script.js
+++ Stylesheet/script.js
@@ -1,11 +1,17 @@
 $(function(){
-  $('.icon').click(function(){
-    //$(this).find('__').
-    //TODO: Maybe add something here when a user clicks on the certain section
+  setNavigation();
+
+ $('.aboutText').click(function(){
+    window.location="../thi-nguyen.github.io/content.html#about";
   });
 
+  $('.experienceText').click(function(){
+    window.location="../thi-nguyen.github.io/content.html#experience";
+  });
 
-
+  $('.contactText').click(function(){
+    window.location="../thi-nguyen.github.io/content.html#contact";
+  });
 
   $('.icon').hover(
     function() {
@@ -15,4 +21,27 @@ $(function(){
       $(this).find('p').removeClass('text-active');
     }
   );
+
+  $('nav a').on("click", function(){
+    $('nav').find('.activeNav').removeClass('active');
+    $(this).addClass("active");
+  });
 });
+
+function setNavigation() {
+    var path = window.location.hash.substring(1);
+
+    switch(path) {
+      case "about":
+        $('.aboutNav').addClass("active");
+        break;
+      case "experience":
+        $('.expNav').addClass("active");
+        break;
+      case "contact":
+        $('.contactNav').addClass("active");
+        break;
+      default:
+
+    }
+}
